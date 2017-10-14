8481b3df40642cca98a5a0f99d6d9dfc880c2f9a
Agustín Santiago Gutiérrez <elsantodel90@gmail.com>
2017-10-14 19:29:17 -0300

    Script to add many users to a contest

---
+++ add-many-users.sh
@@ -0,0 +1,14 @@
+if [[ $# -ne 1 ]]
+then
+    echo "Script should receive a single argument: the contest id"
+    echo "User Pass pairs are supplied through standard input"
+    echo "Input is space separated, so usernames and passwords must not contain spaces"
+    exit
+fi
+
+echo "Adding users to ContestID" $1
+
+while read user pass
+do
+    echo cmsAddUser -c $1 -p $pass $user "" $user
+done
